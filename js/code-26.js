cbe07d65305288e017962a379e5965ccdd8401f7
Jack Ku <95430501+sakujjjj@users.noreply.github.com>
2022-04-18 11:02:09 +0800

    Update code-26.js

--- js/code-26.js
+++ js/code-26.js
@@ -1,6 +1,1 @@
-document.getElementById("myButton").addEventListener("click", function(){
-let imageContainer = document.createElement("div");
-console.log(imageContainer)
-imageContainer.innerHTML = "123";
-document.getElementById("testClick").appendChild(imageContainer);
-})
+
